49151e79225a6cb39784d0402be6e36e68f2005f
Heidi Dong <dong.heidi@gmail.com>
2017-01-24 20:34:07 -0800

    Add functionality to save messages to Firebase

--- app/src/main/java/com/hhsprogramming/firebasemessageboard/MainActivity.java
+++ app/src/main/java/com/hhsprogramming/firebasemessageboard/MainActivity.java
@@ -2,6 +2,12 @@ package com.hhsprogramming.firebasemessageboard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // When postBtn is clicked, call saveMessage()
+        Button btn = (Button) findViewById(R.id.postBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveMessage(v);
+            }
+        });
+    }
+
+    public void saveMessage(View v) {
+        EditText nameField = (EditText) findViewById(R.id.nameField);
+        EditText msgField = (EditText) findViewById(R.id.messageField);
+
+        // Get actual text from the text fields
+        String name = nameField.getText().toString();
+        String msg = msgField.getText().toString();
+
+        // Write a message to the database
+        Message message = new Message(name, msg);
+        DatabaseReference database = FirebaseDatabase.getInstance().getReference();
+        database.child("messages").push().setValue(message);
     }
 }
---
+++ app/src/main/java/com/hhsprogramming/firebasemessageboard/Message.java
@@ -0,0 +1,17 @@
+package com.hhsprogramming.firebasemessageboard;
+
+/**
+ * Created by Heidi on 1/24/2017.
+ */
+
+public class Message {
+
+    public String name;
+    public String message;
+
+    public Message(String name, String message) {
+        this.name = name;
+        this.message = message;
+    }
+
+}
